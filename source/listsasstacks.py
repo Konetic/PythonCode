345a1eb065714d4fc19a0ee9e6e426d0448c62ff
koneabo123 <koneabo123@gmail.com>
2024-05-27 12:40:54 -0700

    removing untracked files

--- source/listsasstacks.py
+++ source/listsasstacks.py
@@ -6,7 +6,7 @@ queue.popleft()                 # The first to arrive now leaves
 'Eric'
 queue.popleft()                 # The second to arrive now leaves
 'John'
-queue                           # Remaining queue in order of arrival
+#queue                           # Remaining queue in order of arrival
 deque(['Michael', 'Terry', 'Graham'])
 print(deque)
 squares = []
